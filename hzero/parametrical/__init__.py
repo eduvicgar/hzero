c30da16c3fe36179612005f611a08f7aa9282cdb
eduvicgar <162564929+eduvicgar@users.noreply.github.com>
2025-06-23 22:04:47 +0200

    init file in parametrical

---
+++ hzero/parametrical/__init__.py
@@ -0,0 +1,6 @@
+from .mean import Mean
+from .variance import Variance
+from .mean_difference import MeanDiff
+from .variances_ratio import VariancesRatio
+
+__all__ = ["Mean", "Variance", "MeanDiff", "VariancesRatio"]
